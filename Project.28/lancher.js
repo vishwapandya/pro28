3c4690134de4b6dde0bb7720143895548d9059e2
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-06-29 11:47:47 +0530

    Add files via upload

---
+++ Project.28/lancher.js
@@ -0,0 +1,27 @@
+class launcher{
+    constructor(body,anchor){
+        var Options = {
+            bodyA:body,
+            pointB:anchor,
+            stiffness:0.004,
+            length:1,
+        }
+          this.bodyA = body;
+          this.pointB = anchor;
+          this.launcher = Constraint.create(Options)
+         World.add(world,this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    fly(){
+        this.launcher.bodyA = null;
+    }
+    display(){
+        if(this.launcher.bodyA){
+            var pointA = this.bodyA.position;
+            var pointB = this.pointB;
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+    }
+}
